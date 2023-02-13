486dd03956fab77d0f285de6db1efb1e156b3972
chris <chrisk13fer@gmail.com>
2023-02-13 09:28:30 +0000

    build_ext

--- setup.py
+++ setup.py
@@ -1,22 +1,23 @@
 from distutils.core import setup
 import os
 from glob import glob
-from pybind11.setup_helpers import Pybind11Extension
+from pybind11.setup_helpers import Pybind11Extension, build_ext
 
-print("src path: ", os.path.abspath(__file__))
+# print("src path: ", os.path.abspath(__file__))
 
 
 cccrt = Pybind11Extension  (
     'cccrt',
     sources=['CCCpy.cpp'],
     depends=['CCC.hpp'],
-    extra_compile_args=['-std=c++17', '-O3', '-DPYTHON_BUILD'],
+    extra_compile_args=['-O3', '-DPYTHON_BUILD'],
     include_dirs=['.','./eigen-3.4.0', './EigenRand'],
 )
 
 setup(
     name='cccrt',
     version='0.9.3',
+    cmdclass={"build_ext": build_ext},
     ext_modules=[cccrt],
     )
 
